1aaaff6753bc0527a9a1c974fd5d2297271499dd
meganeshinaida <156678792+meganeshinaida@users.noreply.github.com>
2025-12-29 23:10:38 +0100

    je commit d'abord c'est apres que je vais chercher pourquoi le code ne marche pas

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,8 +19,12 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
+
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter()
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const formSchema =authFormSchema(type)
@@ -29,14 +33,39 @@ const AuthForm = ({ type }: { type: string }) => {
     defaultValues: {
       email: "",
       password: "",
+      firstName: "",
+    lastName: "",
+    address1: "",
+    city: "",
+    state: "",
+    postalCode: "",
+    dateOfBirth: "",
+    ssn: "",
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
+  const onSubmit=async (data: z.infer<typeof formSchema>)=> {
+    // Do something with the form values.    
     // ✅ This will be type-safe and validated.
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
+    try {
+    // signup with app write & create a plaid token
+    if(type === 'sign-up'){
+      const newUser = await signUp(data)
+      setUser(newUser)
+    }
+    if(type==="sign-in"){
+      // const response = await signIn({
+      // email:data.email,
+      //  password:data.password
+      // })
+      // if(response) router.push('/')
+    }
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setIsLoading(false);
+    }
+    
   }
   return (
     <section className="auth-form">
@@ -95,6 +124,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific Address"
+                  />
+                   <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your City"
                   />
                   <div className="flex gap-4">
                      <CustomInput
--- lib/utils.ts
+++ lib/utils.ts
@@ -198,12 +198,13 @@ export const authFormSchema =(type:string)=>z.object({
   // for both sign in and sign up
  email: z.string().email(),
  password:z.string().min(8),
-// just for sighup
- firstName: type === 'sign-in' ?z.string().optional : z.string().min(3),
- lastName: type === 'sign-in' ?z.string().optional : z.string().min(3),
- address1: type === 'sign-in' ?z.string().optional : z.string().max(50),
- state: type === 'sign-in' ?z.string().optional : z.string().min(3),
- postalCode: type === 'sign-in' ?z.string().optional : z.string().min(3).max(6),
- dateOfBirth: type === 'sign-in' ?z.string().optional : z.string().min(3),
- ssn: type === 'sign-in' ?z.string().optional : z.string().min(3)
+// just for sign up
+ firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+ lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+ address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+ state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
+ city: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
+ postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+ dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+ ssn: type === 'sign-in' ? z.string().optional : z.string().min(3)
 })
